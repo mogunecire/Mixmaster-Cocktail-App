0e2cdefb21f72b7a0e0997203402842812e4a325
mogunecire <emsi.ilija@gmail.com>
2025-10-19 14:46:31 +0200

    first commit

---
+++ src/pages/Cocktail.jsx
@@ -0,0 +1,95 @@
+import axios from 'axios'
+import { useLoaderData, Link, Navigate } from 'react-router-dom'
+import Wrapper from '../assets/wrappers/CocktailPage'
+const singleCocktailUrl =
+  'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i='
+import { useQuery } from '@tanstack/react-query'
+
+const singleCocktailQuery = (id) => {
+  return {
+    queryKey: ['cocktail', id],
+    queryFn: async () => {
+      const { data } = await axios.get(`${singleCocktailUrl}${id}`)
+      return data
+    },
+  }
+}
+export const loader =
+  (queryClient) =>
+  async ({ params }) => {
+    const { id } = params
+
+    await queryClient.ensureQueryData(singleCocktailQuery(id))
+    return { id }
+  }
+const Cocktail = () => {
+  const { id } = useLoaderData()
+  const { data } = useQuery(singleCocktailQuery(id))
+
+  if (!data) return <Navigate to="/" />
+  const singleDrink = data.drinks[0]
+  const {
+    strDrink: name,
+    strDrinkThumb: image,
+    strAlcoholic: info,
+    strCategory: category,
+    strGlass: glass,
+    strInstructions: instructions,
+  } = singleDrink
+  const ingredients = Object.entries(singleDrink)
+    .filter((item) => {
+      const [key, value] = item
+      return key.startsWith('strIngredient') && value !== null
+      // if (/^strIngredient\d+$/.test(key)) return item
+    })
+    .map(([_, value]) => value)
+  console.log(ingredients)
+
+  return (
+    <Wrapper>
+      <header>
+        <Link to="/" className="btn">
+          back home
+        </Link>
+        <h3>{name}</h3>
+      </header>
+      <div className="drink">
+        <img src={image} alt={name} className="img" />
+        <div className="drink-info">
+          <p>
+            <span className="drink-data">name: </span>
+            {name}
+          </p>
+          <p>
+            <span className="drink-data">category: </span>
+            {category}
+          </p>
+          <p>
+            <span className="drink-data">info: </span>
+            {info}
+          </p>
+          <p>
+            <span className="drink-data">glass: </span>
+            {glass}
+          </p>
+          <p>
+            <span className="drink-data">ingredients: </span>
+            {ingredients.map((item, index) => {
+              return (
+                <span className="ing" key={item}>
+                  {item}
+                  {index < ingredients.length - 1 ? ',' : ''}
+                </span>
+              )
+            })}
+          </p>
+          <p>
+            <span className="drink-data">instructions: </span>
+            {instructions}
+          </p>
+        </div>
+      </div>
+    </Wrapper>
+  )
+}
+export default Cocktail
